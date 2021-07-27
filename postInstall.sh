da661a3aa67b51aaa62256e94009c407b8834573
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2021-07-27 12:41:27 -0400

    Create postInstall.sh

---
+++ postInstall.sh
@@ -0,0 +1,4 @@
+keptn install --use-case=continuous-delivery
+
+wget https://raw.githubusercontent.com/keptn-sandbox/monaco-service/0.8.4/deploy/service.yaml
+kubectl apply  -n keptn -f service.yaml
