14025e7f649b610c74f57e78f36552762739871b
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2021-07-27 12:17:45 -0400

    Removed unncessary files

--- keptn_project_template/auto-remediation/generic-executor/action.triggered.validatedns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "This script gets called for the remediation action: $DATA_ACTION_ACTION for PID=$DATA_PROBLEM_PID ($DATA_PROBLEM_PROBLEMTITLE)"
-echo "Full action event available at $1"
-echo "-------------------------------------------------------------------"
-echo "Here some more context for our action: \$PID=${DATA_PROBLEM_PID}, \$SHKEPTNCONTEXT=$SHKEPTNCONTEXT, \$SOURCE=$SOURCE, \$PROJECT=$DATA_PROJECT, \$SERVICE=$DATA_SERVICE, \$STAGE=$DATA_STAGE"
-echo "Lets do some something against problem $DATA_PROBLEM_PROBLEMTITLE..."
